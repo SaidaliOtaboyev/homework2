13f9b899e09fa9aea978a529feafeb06283333d4
SaidaliOtaboyev <161677329+SaidaliOtaboyev@users.noreply.github.com>
2025-11-11 22:47:14 +0500

    Update Account.h

--- Account.h
+++ Account.h
@@ -1,111 +1,69 @@
-#ifndef ACCOUNT_H
-#define ACCOUNT_H
-
 #include <iostream>
 #include <string>
-using namespace std;
 
-// Base class: Account
 class Account {
 protected:
-    string ownerName;
+    std::string ownerName;
     double balance;
 
 public:
-    // Parameterized constructor
-    Account(string name = "", double bal = 0.0) : ownerName(name), balance(bal) {}
-
-    // Getter for balance
-    double getBalance() const {
+    Account(std::string name, double bal) : ownerName(name), balance(bal) {}
+    
+    double getBalance() {
         return balance;
     }
 
-    // Virtual display function
-    virtual void display() const {
-        cout << "Owner: " << ownerName << endl;
-        cout << "Balance: " << balance << endl;
+    virtual void display() {
+        std::cout << "Owner: " << ownerName << "\nBalance: " << balance << std::endl;
     }
 
-    // Virtual destructor
     virtual ~Account() {
-        cout << "Account closed for " << ownerName << endl;
+        std::cout << "Account closed for " << ownerName << std::endl;
     }
 
-    // Operator overloading
-
-    // + operator → combines balances and returns new Account
-    Account operator+(const Account& other) const {
+    Account operator+(const Account& other) {
         return Account(ownerName, balance + other.balance);
     }
 
-    // - operator → returns difference between balances
-    double operator-(const Account& other) const {
+    double operator-(const Account& other) {
         return balance - other.balance;
     }
 
-    // == operator → compares balances
-    bool operator==(const Account& other) const {
+    bool operator==(const Account& other) {
         return balance == other.balance;
     }
 
-    // << operator → output account info
-    friend ostream& operator<<(ostream& os, const Account& acc) {
-        os << "Owner: " << acc.ownerName << endl;
-        os << "Balance: " << acc.balance << endl;
+    friend std::ostream& operator<<(std::ostream& os, const Account& acc) {
+        os << "Owner: " << acc.ownerName << "\nBalance: " << acc.balance;
         return os;
     }
 
-    // >> operator → input account info
-    friend istream& operator>>(istream& is, Account& acc) {
+    friend std::istream& operator>>(std::istream& is, Account& acc) {
         is >> acc.ownerName >> acc.balance;
         return is;
     }
 };
 
-// Derived class: SavingsAccount
 class SavingsAccount : public Account {
 private:
     double interestRate;
 
 public:
-    // Constructor
-    SavingsAccount(string name = "", double bal = 0.0, double rate = 0.0)
-        : Account(name, bal), interestRate(rate) {}
+    SavingsAccount(std::string name, double bal, double rate) : Account(name, bal), interestRate(rate) {}
 
-    // Override display
-    void display() const override {
-        cout << "Owner: " << ownerName << endl;
-        cout << "Balance: " << balance << endl;
-        cout << "Interest Rate: " << interestRate << "%" << endl;
-    }
-
-    // Destructor
-    ~SavingsAccount() override {
-        cout << "Account closed for " << ownerName << endl;
+    void display() override {
+        std::cout << "Owner: " << ownerName << "\nBalance: " << balance << "\nInterest Rate: " << interestRate << "%" << std::endl;
     }
 };
 
-// Derived class: CheckingAccount
 class CheckingAccount : public Account {
 private:
     double transactionFee;
 
 public:
-    // Constructor
-    CheckingAccount(string name = "", double bal = 0.0, double fee = 0.0)
-        : Account(name, bal), transactionFee(fee) {}
+    CheckingAccount(std::string name, double bal, double fee) : Account(name, bal), transactionFee(fee) {}
 
-    // Override display
-    void display() const override {
-        cout << "Owner: " << ownerName << endl;
-        cout << "Balance: " << balance << endl;
-        cout << "Transaction Fee: " << transactionFee << endl;
-    }
-
-    // Destructor
-    ~CheckingAccount() override {
-        cout << "Account closed for " << ownerName << endl;
+    void display() override {
+        std::cout << "Owner: " << ownerName << "\nBalance: " << balance << "\nTransaction Fee: " << transactionFee << std::endl;
     }
 };
-
-#endif
